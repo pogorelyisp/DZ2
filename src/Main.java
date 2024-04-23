e52c4673c4482d1d8d2ab9da02b7cd684d013b03
Pogorelyisp <pogorelyisp@yandex.ru>
2024-04-23 16:40:35 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
-        int account = 0;            // Счет клиента
-        int SumPopoln = 1200;         // Сумма пополнения
-        int Bonus = (SumPopoln/100);  // Расчет бонусных рублей
-        int Endaccount = (account + SumPopoln);  // Счет клиента после пополнения
 
-        if (SumPopoln < 1000) {
-            System.out.println("Итоговый счет " + Endaccount);
+        int account = 100;            // Счет клиента
+        int deposit = 1200;         // Сумма пополнения
+        int bonus = (deposit/100);  // Расчет бонусных рублей
+        int finalaccount = (account + deposit);  // Счет клиента после пополнения
+
+        if (deposit < 1000) {
+            System.out.println("Итоговый счет " + finalaccount);
         } else {
-            System.out.println("Итоговый счет " + (Endaccount + Bonus));
-            System.out.println("Вы получили дополнительные бонусы " + Bonus);
+            System.out.println("Итоговый счет " + (finalaccount + bonus));
+            System.out.println("Вы получили дополнительные бонусы " + bonus);
         }
 
 
 
-        //System.out.println("Итоговый счет " + Endaccount);
+
 
 
 
